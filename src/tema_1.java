525efce6ddb14e956471656baca2edb6ba3decc1
Marton Aaron <aaron@webgurus.biz>
2017-03-23 21:13:59 +0200

    Lab 4 - tema 1 "you are given a temperature in F; covert it in C; C= 5/9 *(F-32)"

---
+++ src/tema_1.java
@@ -0,0 +1,18 @@
+import javax.swing.plaf.basic.BasicInternalFrameTitlePane;
+import java.text.DecimalFormat;
+
+/**
+ * Created by aaronmarton on 23/03/2017.
+ */
+public class tema_1 {
+    public static void main(String[] args) {
+
+        double temperature_f = SkeletonJava.readDoubleConsole("Enter temperature in Fahrenheit: ");
+        double temperature_c;
+        DecimalFormat numberFormat = new DecimalFormat("#.0");
+
+        temperature_c = ((float)5/9) * (temperature_f - 32);
+
+        System.out.println("\n" + numberFormat.format(temperature_f)  + " Fahrenheit = " + numberFormat.format(temperature_c) + " Celsius");
+    }
+}
