85eb67e56456f7e4371e1426427995d219dd87b5
Marton Aaron <aaron@webgurus.biz>
2017-03-24 09:10:02 +0200

    Lab 4 - tema 6 CORRECTED - "implement a table of conversion F to C for common temperatures that a tourist might find it useful (use a for loop and apply the formula from above)"

--- src/tema_4.java
+++ src/tema_4.java
@@ -9,17 +9,11 @@ import java.text.DecimalFormat;
 public class tema_4 {
     public static void main(String[] args) {
         int[] list_of_numbers = {3, 45, 32, 12, 34, 5, 66, 7, 43, 67, 87, 5, 2};
-        int max = 0;
+        int max = list_of_numbers[0];
 
-        for (int i = 0; i < list_of_numbers.length; i++) {
-
-            list_of_numbers[i] = max;
-
-            for (int j = i+1; j < list_of_numbers.length; j++) {
-
-                if (list_of_numbers[j] > max) {
-                    max = list_of_numbers[j];
-                }
+        for (int i : list_of_numbers) {
+            if (i > max) {
+                max = i;
             }
         }
 
